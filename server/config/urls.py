cef206b5300473895d06c428ea051e09b1ae9ee8
parabola <52104201+ku0629@users.noreply.github.com>
2024-11-30 22:46:51 +0900

    feat: add login and signup (#4)
    * fix: functions about user
    
    * feat: add login and signup
    
    * refactor: change server file directory structure
    
    * fix: functions about user
    
    * feat: add login and signup
    
    ---------
    
    Co-authored-by: Jaehyun Yoon <yuchem2@gmail.com>

--- server/config/urls.py
+++ server/config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/userdata/",include("userdata.urls"),)
+    path("api/v1/userdata/",include("userdata.urls")),
+    path("api/v1/users/", include("users.urls")),
 ]
