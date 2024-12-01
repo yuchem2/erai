dad5037b3ef17e1eb9b84d7760cd6dbbbf439528
Jaehyun Yoon <yuchem2@gmail.com>
2024-12-01 11:31:45 +0900

    feat: add prompt logic (#9)

--- web/src/component/pages/prompt.tsx
+++ web/src/component/pages/prompt.tsx
@@ -1,12 +1,42 @@
 'use client'
 
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { Box, TextField, Typography } from '@mui/material'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useMutation } from '@tanstack/react-query'
+import { postPrompt } from '@/api/prompt'
+
+interface Inputs {
+    prompt: string
+}
 
 export default function Prompt() {
-    const [inputValue, setInputValue] = useState('')
+    const { register, handleSubmit } = useForm<Inputs>()
+    const [userData, setUserData] = useState('')
+    const mutation = useMutation({
+        mutationFn: (request: { prompt: string }) => {
+            return postPrompt(request)
+        },
+        onSuccess: (data: string) => {
+            console.log(data)
+            setUserData(data)
+        },
+    })
 
-    // TODO: add chat bot logic
+    const onSubmit: SubmitHandler<Inputs> = async (data: Inputs) => {
+        const request = {
+            prompt: data.prompt,
+        }
+        mutation.mutate(request)
+    }
+
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            e.preventDefault()
+            handleSubmit(onSubmit)()
+        }
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen w-full">
             <Typography
@@ -29,12 +59,13 @@ export default function Prompt() {
                     maxWidth: '700px', // 입력창 너비 제한
                 }}
             >
+                {userData && <Typography>{userData}</Typography>}
                 <TextField
                     variant="outlined"
                     fullWidth
                     placeholder="메시지 to ERAI"
-                    value={inputValue}
-                    onChange={e => setInputValue(e.target.value)}
+                    {...register('url')}
+                    onKeyDown={handleKeyDown}
                     sx={{
                         backgroundColor: '#646464',
                         borderRadius: '15px',
